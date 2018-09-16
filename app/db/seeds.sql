05d012da5adeec07584c1df06bec8c120bc1a093
Madhu <mchavali05@gmail.com>
2018-09-15 23:45:28 -0700

    add data

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -4,3 +4,10 @@ VALUES ('Ceasar', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'https://images.pexels.com/phot
 ('Oscar', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&h=150'),
 ('Smokey', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://4.imimg.com/data4/AI/KC/MY-30251834/aquarium-fish-250x250.jpg'),
 ('Flappy', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/133394/pexels-photo-133394.jpeg?auto=compress&cs=tinysrgb&h=150');
+
+INSERT INTO users (name, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10, photo) 
+VALUES ('Monty', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'https://images.pexels.com/photos/38268/snake-mamba-green-mamba-toxic-38268.jpeg?auto=compress&cs=tinysrgb&h=150'),
+('Pussy', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=150'),
+('Tigger', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/46251/sumatran-tiger-tiger-big-cat-stripes-46251.jpeg?auto=compress&cs=tinysrgb&h=150'),
+('Kea', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Kea_on_rock_while_snowing.jpg/150px-Kea_on_rock_while_snowing.jpg'),
+('Bek', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'http://www.nzfrogs.org/site/nzfrog/images/Frog_photos/hochstettersleft.gif');
