1330156ba212d3318c298416088af1c0248978fd
Madhu <mchavali05@gmail.com>
2018-09-15 23:14:51 -0700

    add data

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,4 +1,6 @@
 INSERT INTO users (name, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10, photo) 
-VALUES ('Ceasar', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'https://images.pexels.com/photos/248307/pexels-photo-248307.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-('Misty', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/62321/kitten-cat-fluffy-cat-cute-62321.jpeg?auto=compress&cs=tinysrgb&h=350'),
-('Oscar', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&h=350');
+VALUES ('Ceasar', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'https://images.pexels.com/photos/248307/pexels-photo-248307.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=150'),
+('Misty', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/62321/kitten-cat-fluffy-cat-cute-62321.jpeg?auto=compress&cs=tinysrgb&h=150'),
+('Oscar', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&h=150'),
+('Smokey', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://4.imimg.com/data4/AI/KC/MY-30251834/aquarium-fish-250x250.jpg'),
+('Flappy', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://images.pexels.com/photos/133394/pexels-photo-133394.jpeg?auto=compress&cs=tinysrgb&h=150');
