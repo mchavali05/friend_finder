47544df596b6723559ac39915f1930b70daa7c75
Madhu <mchavali05@gmail.com>
2018-09-15 22:28:31 -0700

    update seeds data

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (name, score1, score2, score3, score4, score5, score6, score7, score8, score9, score10, photo) 
-VALUES ('Joe Smith', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'http://mlb.mlb.com/mlb/images/players/head_shot/501925.jpg'),
-('Joe1 Smith1', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://nsidc.org/sites/nsidc.org/files/tanner.jpg'),
-('Joe2 Smith2', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://avatars2.githubusercontent.com/u/8504998?v=3&s=460');
+VALUES ('Ceasar', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'https://images.pexels.com/photos/248307/pexels-photo-248307.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+('Misty', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://cdn.pixabay.com/photo/2015/11/16/22/14/cat-1046544_1280.jpg'),
+('Oscar', 1, 4, 6, 4, 5, 6, 7, 5, 2, 15, 'https://cdn.pixabay.com/photo/2016/12/13/00/13/rabbit-1903016_1280.jpg');
